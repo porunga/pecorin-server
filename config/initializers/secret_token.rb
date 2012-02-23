b47c1a6fd6b7915e86d02bf7a19e2bde4f44dad6
kaneoka <tokaneoka@gmail.com>
2012-02-23 23:26:31 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PecorinServer::Application.config.secret_token = '9d623209c8106adc0b92b6250c37567a36d62dbde0f55a32311d30a7bd0c82bc3cbb6748bfcaa3165619852afa14b79057c740f7beb72e698ee079a45996e67b'
