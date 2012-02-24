e431fa4e90d6e7d18a5f57b06e6e7480cfc0d559
takahashi <root@ip-10-150-169-155.ap-northeast-1.compute.internal>
2012-02-25 00:56:33 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -56,16 +56,17 @@ PecorinServer::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-  match ':controller(/:action(/:id))(.:format)'
 
   match '/auth/facebook/callback' => 'sessions#create'
   match '/auth/failure' => 'sessions#failure'
   match '/login' => 'sessions#new', :as => :login
-  match '/logout' => 'session#destroy', :as => :logout
+  match '/logout' => 'sessions#destroy', :as => :logout
 
   resources :device_registrations, :only => :create
   resources :pecori, :only => :create
 
   put 'user/:uid/location', :to => 'location#update', :via => :put
+
+  match ':controller(/:action(/:id))(.:format)'
 
 end
