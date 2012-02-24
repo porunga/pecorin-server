42ca319ddbc6680ecd21fb5df35ca7808a42c002
takahashi <root@ip-10-150-169-155.ap-northeast-1.compute.internal>
2012-02-24 16:12:46 +0900

    Login function returns json which include facebook_id and name

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,23 @@
+require 'json'
+
 class SessionsController < ApplicationController
 
 	def create
 		auth = request.env["omniauth.auth"]
 		@user = User.where(:facebook_id => auth['uid']).first || User.create_with_omniauth(auth)
 		session[:user_id] = @user.id
+		redirect_to :action => "success"
+	end
+
+	def success
+		user = User.where(:id => session[:user_id]).first || nil
+		response = {:name => user.name, :facebook_id => user.facebook_id}
+		@json = response.to_json
 	end
 
 	def failure
-		#redirect_to "/", :alert => "Authentication error: #{params[:message].humanize}"
+		response = {:error => params[:message].humanize}
+		@json = response.to_json
 	end
 
 	def new
--- config/routes.rb
+++ config/routes.rb
@@ -55,6 +55,7 @@ PecorinServer::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  match ':controller(/:action(/:id))(.:format)'
 
   match '/auth/facebook/callback' => 'sessions#create'
   match '/auth/failure' => 'sessions#failure'
